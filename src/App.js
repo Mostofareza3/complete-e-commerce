7916234da997b03c76b71019c83cac0f79ff590c
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-10-16 22:13:39 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from 'react-router-bootstrap';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -10,17 +12,28 @@ import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <Router>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header >
-          <a href="/">amazon</a>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Switch>
-            <Route path="/product/:slug"> <ProductScreen /> </Route>
-            <Route path="/" > <HomeScreen /></Route>
-          </Switch>
+          <Container>
+            <Switch>
+              <Route path="/product/:slug"> <ProductScreen /> </Route>
+              <Route path="/" > <HomeScreen /></Route>
+            </Switch>
+          </Container>
 
         </main>
+        <footer>
+          <div className="text-center">All rights reserved Mostofa Reza</div>
+        </footer>
       </div>
     </Router>
 
