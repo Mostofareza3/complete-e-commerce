121a3bca3618b8367c5f3e87288fcc6f943f8dbc
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-10-16 23:53:01 +0600

    product component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -16,7 +17,6 @@ const reducer = (state, action) => {
 }
 
 const HomeScreen = () => {
-    // const [products, setProducts] = useState([]);
     const [{ loading, error, products }, dispatch] = useReducer(reducer, {
         products: [],
         loading: true,
@@ -39,22 +39,16 @@ const HomeScreen = () => {
             <h1>Featured Products</h1>
             <div className="products">
                 {
-                    loading ? <div>loading...</div> : error ? <div>{error}</div> :
-                        products.map(product => (
-                            <div className="product" key={product.slug}>
-                                <Link to={`/product/${product.slug}`}>
-                                    <img src={product.image} alt={product.name} />
-                                </Link>
-                                <div className="product-info">
-                                    <Link to={`/product/${product.slug}`}>
-                                        <p>{product.name} </p>
-                                    </Link>
-                                    <p><strong>${product.price}</strong></p>
-                                    <button>Add to cart</button>
-                                </div>
-                            </div>
-                        ))
-                }
+                    loading ? (<div>loading...</div>) : error ? (<div>{error}</div>) :
+                        (
+                            <Row>
+                                {products.map((product) => (
+                                    <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                        <Product product={product}></Product>
+                                    </Col>
+                                ))}
+                            </Row>
+                        )}
             </div>
         </div>
     );
