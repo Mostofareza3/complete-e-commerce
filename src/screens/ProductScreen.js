0d79d36326b5df8357bfa3924c54ce1cb95096cf
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-10-18 23:02:39 +0600

    mongodb done

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -52,7 +52,7 @@ const ProductScreen = () => {
     const addToCartHandler = async () => {
         const existItem = cart.cartItems.find((x) => x._id === product._id);
         const quantity = existItem ? existItem.quantity + 1 : 1;
-        const { data } = await axios.get(`/api/products/${product._id}`)
+        const { data } = await axios.get(`http://localhost:5000/api/products/${product._id}`)
         if (data.countInStock < quantity) {
             window.alert('Sorry! Product is out of stock!');
             return;
